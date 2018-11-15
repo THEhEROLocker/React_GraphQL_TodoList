15b98e4d6f7180c4414184d05c9b09da1726c164
Rohan Elukurthy <roh3@pdx.edu>
2018-11-14 17:43:41 -0800

    working local GraphQl server with Prisma

---
+++ todolist-backend/src/index.js
@@ -0,0 +1,37 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding')
+
+const resolvers = {
+ Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        todoes: (root, args, context, info) => {
+            return context.db.query.todoes({}, info)
+        } 
+ }
+}
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    resolverValidationOptions :{
+        requireResolversForResolveType: false
+    },
+    context: req => ({
+            ...req,
+            db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://todolist-server-acc0d23815.herokuapp.com/list/dev',
+            secret: process.env.TODOKEY,
+            debug: true
+        }),
+    }),
+})
+
+server.start({
+"cors": {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+},
+"port": process.env.port || 4000
+},() => console.log("server"))
