d5c99cb2f3f18541b4f03181132bc428920f2614
Rohan Elukurthy <roh3@pdx.edu>
2018-11-15 09:58:01 -0800

    deployed app to netlify

--- todolist-view/src/index.js
+++ todolist-view/src/index.js
@@ -7,7 +7,6 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import gql from 'graphql-tag'
 
 import * as serviceWorker from './serviceWorker';
 
